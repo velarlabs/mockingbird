735e0e1107cf4579d3b23c9501f765042a15aff7
Shakil Ahmed <shakilahmmeed@gmail.com>
2023-03-07 15:16:32 +0600

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import * as ReactDOM from "react-dom";
+import Home from "./pages/home";
+
+function render() {
+	ReactDOM.render(
+		<div>
+			<Home />
+		</div>,
+		document.body
+	);
+}
+
+render();
---
+++ src/pages/home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div>
+			<h1>This is homepage</h1>
+		</div>
+	);
+}
