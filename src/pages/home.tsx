2fb906f81d4208aff4c87019cd7f356e823910fd
Uthsob Chakraborty <uthsob9@gmail.com>
2023-12-04 14:19:01 +0600

    Loading Screen Animation added (#7)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,42 @@
+import React, { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 
+
+const Loader = () => (
+	<div className="flex h-screen items-center justify-center">
+		<div className="text-4xl animate-bounce">🚀</div>
+		<div className="ml-5">
+			<p className="text-lg font-bold">Launching...</p>
+			<p className="text-gray-500">Blast off in progress</p>
+		</div>
+	</div>
+);
+
 export default function Home() {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 3000);
+	}, []);
+
+
 	return (
 		<div>
-			<Navbar />
+			{isLoading ? (
+				<Loader />
+			) : (<div>
+				<Navbar />
 
-			<div>
-				<Sidebar />
+				<div>
+					<Sidebar />
+				</div>
 			</div>
-		</div>
+
+			)
+			}
+		</div >
 	);
 }
