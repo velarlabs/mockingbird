92a9e677e7ea487fcf02814df985b895a2e52106
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2023-12-07 12:16:23 +0600

    removed commented list

--- src/pages/home/views/EndpointLists.tsx
+++ src/pages/home/views/EndpointLists.tsx
@@ -52,12 +52,15 @@ export default function EndpointLists({
             onClick={() => setSelectedEndpointIDX(idx)}
           >
             <span
-              className={cn("px-2 py-1 w-16 text-center text-white bg-green-400 rounded-md", {
-                "bg-green-400": method === "GET",
-                "bg-blue-400": method === "POST",
-                "bg-yellow-400": method === "PUT",
-                "bg-red-400": method === "DELETE",
-              })}
+              className={cn(
+                "px-2 py-1 w-16 text-center text-white bg-green-400 rounded-md",
+                {
+                  "bg-green-400": method === "GET",
+                  "bg-blue-400": method === "POST",
+                  "bg-yellow-400": method === "PUT",
+                  "bg-red-400": method === "DELETE",
+                }
+              )}
             >
               {method.toUpperCase()}
             </span>
@@ -68,109 +71,3 @@ export default function EndpointLists({
     </div>
   );
 }
-
-// const endpointData = [
-//   {
-//     endpoint: "/api/users",
-//     method: "GET",
-//     description: "Get a list of users",
-//     mockResponse: [
-//       { id: 1, username: "john_doe", email: "john@example.com" },
-//       { id: 2, username: "jane_smith", email: "jane@example.com" },
-//     ],
-//   },
-//   {
-//     endpoint: "/api/users/:id",
-//     method: "GET",
-//     description: "Get user details by ID",
-//     mockResponse: { id: 1, username: "john_doe", email: "john@example.com" },
-//   },
-//   {
-//     endpoint: "/api/posts",
-//     method: "POST",
-//     description: "Create a new post",
-//     sampleRequest: {
-//       title: "New Post",
-//       content: "This is the content of the post.",
-//     },
-//     mockResponse: { success: true, message: "Post created successfully" },
-//   },
-//   {
-//     endpoint: "/api/posts/:id",
-//     method: "GET",
-//     description: "Get details of a specific post by ID",
-//     mockResponse: {
-//       id: 1,
-//       title: "New Post",
-//       content: "This is the content of the post.",
-//     },
-//   },
-//   {
-//     endpoint: "/api/posts/:id/comments",
-//     method: "GET",
-//     description: "Get comments for a specific post by ID",
-//     mockResponse: [
-//       { id: 1, userId: 1, comment: "Great post!" },
-//       { id: 2, userId: 2, comment: "Interesting insights." },
-//     ],
-//   },
-//   {
-//     endpoint: "/api/posts/:id/comments",
-//     method: "POST",
-//     description: "Add a new comment to a post",
-//     sampleRequest: { userId: 3, comment: "I have a question." },
-//     mockResponse: { success: true, message: "Comment added successfully" },
-//   },
-//   {
-//     endpoint: "/api/categories",
-//     method: "GET",
-//     description: "Get a list of post categories",
-//     mockResponse: ["Technology", "Science", "Travel"],
-//   },
-//   {
-//     endpoint: "/api/settings",
-//     method: "PUT",
-//     description: "Update user settings",
-//     sampleRequest: { theme: "dark", notifications: true },
-//     mockResponse: { success: true, message: "Settings updated successfully" },
-//   },
-//   {
-//     endpoint: "/api/profile",
-//     method: "GET",
-//     description: "Get user profile information",
-//     mockResponse: {
-//       username: "john_doe",
-//       email: "john@example.com",
-//       avatar: "avatar.jpg",
-//     },
-//   },
-//   {
-//     endpoint: "/api/profile",
-//     method: "PUT",
-//     description: "Update user profile information",
-//     sampleRequest: { email: "john_new@example.com" },
-//     mockResponse: { success: true, message: "Profile updated successfully" },
-//   },
-//   {
-//     endpoint: "/api/orders",
-//     method: "GET",
-//     description: "Get a list of user orders",
-//     mockResponse: [
-//       { orderId: "123", product: "Laptop", quantity: 2 },
-//       { orderId: "124", product: "Smartphone", quantity: 1 },
-//     ],
-//   },
-//   {
-//     endpoint: "/api/orders",
-//     method: "POST",
-//     description: "Place a new order",
-//     sampleRequest: { product: "Tablet", quantity: 3 },
-//     mockResponse: { success: true, message: "Order placed successfully" },
-//   },
-//   {
-//     endpoint: "/api/logout",
-//     method: "POST",
-//     description: "Logout user",
-//     mockResponse: { success: true, message: "Logout successful" },
-//   },
-// ];
